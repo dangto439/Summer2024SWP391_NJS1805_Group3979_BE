fa733e2316ff959671193b431ac3a8d9a8f32955
dangto <dangthse172732@fpt.edu.vn>
2024-05-26 16:20:07 +0700

    send-email

--- src/main/java/com/group3979/badmintonbookingbe/api/AuthenticationAPI.java
+++ src/main/java/com/group3979/badmintonbookingbe/api/AuthenticationAPI.java
@@ -1,5 +1,7 @@
 package com.group3979.badmintonbookingbe.api;
 
+import com.group3979.badmintonbookingbe.model.EmailDetail;
+import com.group3979.badmintonbookingbe.service.EmailService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,29 +21,44 @@ import com.group3979.badmintonbookingbe.service.AuthenticationService;
 @RequestMapping("api")
 @SecurityRequirement(name = "api")
 public class AuthenticationAPI {
-    
+
     // nhận request từ front-end
     @Autowired
     private AuthenticationService authenticationService;
+
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping("register")
     public ResponseEntity register(@RequestBody RegisterRequest registerRequest) {
         Account account = authenticationService.register(registerRequest);
         return ResponseEntity.ok(account);
     }
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequest loginRequest) {
         Account account = authenticationService.login(loginRequest);
         return ResponseEntity.ok(account);
     }
+
     @GetMapping("/test")
     public ResponseEntity test() {
         return ResponseEntity.ok("Hello World");
     }
+
     @GetMapping("/admin-only")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity testAdmin() {
         return ResponseEntity.ok("Admin!!!!!!!!!!!!");
     }
-   
+
+    @GetMapping("/send-email")
+    public void sendMail() {
+        EmailDetail emailDetail = new EmailDetail();
+        emailDetail.setRecipient("dangto0898494156@gmail.com");
+        emailDetail.setSubject("Welcome");
+        emailDetail.setMsgBody("Welcome to my website");
+        emailService.sendMailTemplate(emailDetail);
+    }
+
 }
---
+++ src/main/java/com/group3979/badmintonbookingbe/service/EmailService.java
@@ -0,0 +1,43 @@
+package com.group3979.badmintonbookingbe.service;
+
+import com.group3979.badmintonbookingbe.model.EmailDetail;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    public void sendMailTemplate(EmailDetail emailDetail){
+        try{
+            Context context = new Context();
+
+            context.setVariable("name", "Datsan3979");
+
+            String text = templateEngine.process("emailtemplate", context);
+
+            // Creating a simple mail message
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+
+            // Setting up necessary details
+            mimeMessageHelper.setFrom("admin@gmail.com");
+            mimeMessageHelper.setTo(emailDetail.getRecipient());
+            mimeMessageHelper.setText(text, true);
+            mimeMessageHelper.setSubject(emailDetail.getSubject());
+            javaMailSender.send(mimeMessage);
+        }catch (MessagingException messagingException){
+            messagingException.printStackTrace();
+        }
+    }
+}
