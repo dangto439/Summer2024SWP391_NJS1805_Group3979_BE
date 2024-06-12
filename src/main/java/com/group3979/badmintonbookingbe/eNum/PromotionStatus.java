d9e51e8d2c2bd1b0501020bc9a28590d0e5c3026
minhduc <tommy.minhduck68@gmail.com>
2024-06-12 22:10:32 +0700

    feat(CRUD Promotion)

---
+++ src/main/java/com/group3979/badmintonbookingbe/eNum/PromotionStatus.java
@@ -0,0 +1,6 @@
+package com.group3979.badmintonbookingbe.eNum;
+
+public enum PromotionStatus {
+    ACTIVE,
+    EXPIRED
+}
---
+++ src/main/java/com/group3979/badmintonbookingbe/model/request/PromotionRequest.java
@@ -0,0 +1,18 @@
+package com.group3979.badmintonbookingbe.model.request;
+
+import com.group3979.badmintonbookingbe.eNum.PromotionStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class PromotionRequest {
+    private String promotionCode;
+    private float discount;
+    private Date startDate;
+    private Date endDate;
+    private PromotionStatus status;
+}
