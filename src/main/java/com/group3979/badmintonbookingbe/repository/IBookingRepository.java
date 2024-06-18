d472e4b34fb65bbc54086df2237bfef575454802
Tai Ha <huytainp@gmail.com>
2024-06-18 11:46:28 +0700

    update Booking API

--- src/main/java/com/group3979/badmintonbookingbe/repository/IBookingRepository.java
+++ src/main/java/com/group3979/badmintonbookingbe/repository/IBookingRepository.java
@@ -1,10 +1,15 @@
 package com.group3979.badmintonbookingbe.repository;
 
+import com.group3979.badmintonbookingbe.entity.Account;
 import com.group3979.badmintonbookingbe.entity.Booking;
+import com.group3979.badmintonbookingbe.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IBookingRepository extends JpaRepository<Booking, Long> {
     Booking findByBookingId(Long bookingId);
+    List<Booking> findBookingByAccountAndClub(Account account, Club club);
 }
