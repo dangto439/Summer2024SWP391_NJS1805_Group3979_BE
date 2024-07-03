b44b5726d2c31427d583411d29161aa89cebd269
Tai Ha <huytainp@gmail.com>
2024-07-03 16:49:08 +0700

    create updateScoreAPI update nextRound

--- src/main/java/com/group3979/badmintonbookingbe/service/ContestService.java
+++ src/main/java/com/group3979/badmintonbookingbe/service/ContestService.java
@@ -5,6 +5,7 @@ import com.group3979.badmintonbookingbe.entity.Club;
 import com.group3979.badmintonbookingbe.entity.Contest;
 import com.group3979.badmintonbookingbe.exception.CustomException;
 import com.group3979.badmintonbookingbe.model.request.ContestRequest;
+import com.group3979.badmintonbookingbe.model.response.ContestResponse;
 import com.group3979.badmintonbookingbe.repository.IClubRepository;
 import com.group3979.badmintonbookingbe.repository.IContestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,9 @@ public class ContestService {
     IClubRepository clubRepository;
     @Autowired
     IContestRepository contestRepository;
-    public void createContest(ContestRequest contestRequest){
+    @Autowired
+    GameService gameService;
+    public ContestResponse createContest(ContestRequest contestRequest){
         Club club = clubRepository.findByClubId(contestRequest.getClubId());
         if(club != null){
             Contest contest = new Contest();
@@ -28,9 +31,23 @@ public class ContestService {
             contest.setParticipationPrice(contestRequest.getParticipationPrice());
             contest.setStartDate(contestRequest.getStartDate());
             contest.setContestStatus(ContestStatus.ACTIVE);
-            contestRepository.save(contest);
+            contest = contestRepository.save(contest);
+            gameService.createMatchesContest(contest.getCapacity(), contest);
+            return this.buildContestResponse(contest);
         }else {
             throw new CustomException("Câu lạc bộ không tồn tại");
         }
+    }
+    public ContestResponse buildContestResponse(Contest contest){
+        return ContestResponse.builder()
+                .clubId(contest.getClub().getClubId())
+                .contestId(contest.getContestId())
+                .firstPrize(contest.getFirstPrize())
+                .secondPrize(contest.getSecondPrize())
+                .startDate(contest.getStartDate())
+                .urlBanner(contest.getUrlBanner())
+                .capacity(contest.getCapacity())
+                .participationPrice(contest.getParticipationPrice())
+                .build();
     }
 }
