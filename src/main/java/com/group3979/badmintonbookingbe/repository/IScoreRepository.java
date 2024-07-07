bf3dfbaff72fa83f8f23b570281c8c23c21765d6
minhduc <tommy.minhduck68@gmail.com>
2024-07-08 03:45:38 +0700

    fix(Score): PUT(UpdateScore)

--- src/main/java/com/group3979/badmintonbookingbe/repository/IScoreRepository.java
+++ src/main/java/com/group3979/badmintonbookingbe/repository/IScoreRepository.java
@@ -1,5 +1,6 @@
 package com.group3979.badmintonbookingbe.repository;
 
+import com.group3979.badmintonbookingbe.eNum.SetNumber;
 import com.group3979.badmintonbookingbe.entity.Game;
 import com.group3979.badmintonbookingbe.entity.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import java.util.List;
 @Repository
 public interface IScoreRepository extends JpaRepository<Score,Long> {
     List<Score> findScoresByGame(Game game);
+    void removeBySetNumberAndGame(SetNumber setNumber, Game game);
 }
+
