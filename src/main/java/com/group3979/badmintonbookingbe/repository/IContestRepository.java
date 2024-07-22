52c5468d2c0b96a4d0fbc1d3c9b59b1dce67a561
Tai Ha <huytainp@gmail.com>
2024-07-22 19:44:04 +0700

    create get 10 club hottest api

--- src/main/java/com/group3979/badmintonbookingbe/repository/IContestRepository.java
+++ src/main/java/com/group3979/badmintonbookingbe/repository/IContestRepository.java
@@ -4,6 +4,7 @@ import com.group3979.badmintonbookingbe.eNum.ContestStatus;
 import com.group3979.badmintonbookingbe.entity.Account;
 import com.group3979.badmintonbookingbe.entity.Contest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,8 @@ public interface IContestRepository extends JpaRepository<Contest,Long> {
     Contest findByContestId(Long contestId);
     List<Contest> findContestsByClub_Account(Account account);
     List<Contest> findContestsByContestStatus(ContestStatus contestStatus);
+    @Query("SELECT a \n" +
+            "FROM Contest a \n" +
+            "ORDER BY a.firstPrize DESC")
+    List<Contest> findHotContest();
 }
